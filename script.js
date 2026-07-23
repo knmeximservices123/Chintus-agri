5c8282ce7de19bb1f2b313c6096d04472a5d8a10
knmeximservices123 <knmeximservices@gmail.com>
2026-07-23 23:15:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// Chintus Agri Website
 
+window.onload = function () {
+    console.log("Welcome to Chintus Agri");
+};
+
+// Order confirmation
+function orderNow(product) {
+    alert("Thank you! You selected: " + product);
+
+    window.open(
+        "https://wa.me/919095652696?text=Hello, I want to order " +
+        encodeURIComponent(product),
+        "_blank"
+    );
+}
